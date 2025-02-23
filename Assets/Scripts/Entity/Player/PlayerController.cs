d67afb2b8451e2ab428a1a71a5ce15e41400145b
Archie-Murray <76486169+Archie-Murray@users.noreply.github.com>
2025-02-23 02:32:16 +0000

    feat: Implemented initial dash mechanics

--- Assets/Scripts/Entity/Player/PlayerController.cs
+++ Assets/Scripts/Entity/Player/PlayerController.cs
@@ -10,14 +10,20 @@ namespace Entity.Player {
     public class PlayerController : MonoBehaviour {
         [SerializeField] private float _speed = 5f;
         [SerializeField] private float _jumpForce = 10f;
+        [SerializeField] private float _dashForce = 10f;
+        [SerializeField] private float _dashTime = 1f;
         [SerializeField] private bool _isGrounded = false;
         [SerializeField] private bool _canJump = false;
+        [SerializeField] private bool _canDash = true;
         [SerializeField] private LayerMask _ground;
         [SerializeField] private bool _isJumping = false;
+
+        [SerializeField] private bool _isDashing = false;
 
         [SerializeField] private bool _coyoteTimeTrigger = false;
 
         [SerializeField] private CountDownTimer _coyoteTimer = new CountDownTimer(0.25f);
+        [SerializeField] private CountDownTimer _dashTimer = new CountDownTimer(0f);
 
         private Rigidbody2D _rb2D;
         private SpriteRenderer _renderer;
@@ -27,6 +33,8 @@ namespace Entity.Player {
             _renderer = GetComponent<SpriteRenderer>();
             _coyoteTimer.OnTimerStart += () => _canJump = true;
             _coyoteTimer.OnTimerStop += () => _canJump = false;
+            _dashTimer.OnTimerStart += () => { _canDash = false; _isDashing = true; };
+            _dashTimer.OnTimerStop += () => { _canDash = true; _isDashing = false; };
         }
 
         void FixedUpdate() {
@@ -44,31 +52,43 @@ namespace Entity.Player {
                 _coyoteTimeTrigger = false;
             }
             _coyoteTimer.Update(Time.fixedDeltaTime);
-            if (_coyoteTimer.IsFinished) {
-                _coyoteTimer.Stop();
-            }
+            _dashTimer.Update(Time.fixedDeltaTime);
             _canJump = _isGrounded || _coyoteTimer.IsRunning;
             float input = Input.GetAxisRaw("Horizontal");
-            if (Input.GetKeyDown(KeyCode.Space) && _canJump && !_isJumping) {
+            if (Input.GetKey(KeyCode.Space) && _canJump && !_isJumping) {
                 _rb2D.velocity += Vector2.up * _jumpForce;
                 _canJump = false;
                 _isJumping = true;
-                Debug.Log("Jump");
             }
-            if (_isGrounded) {
-                _rb2D.velocity = new Vector2(input * _speed, _rb2D.velocity.y);
-            } else {
-                _rb2D.velocity = new Vector2(input * _speed, _rb2D.velocity.y);
+            if (Input.GetKey(KeyCode.LeftShift) && _canDash && input != 0.0f) {
+                _dashTimer.Reset(_dashTime);
+                _rb2D.velocity = Vector2.right * input * _dashForce;
+                _isJumping = false;
             }
-            if (_rb2D.velocity.y < 0 && !_isGrounded) {
-                _rb2D.gravityScale = 2f;
+            if (!_isDashing) {
+                if (_isGrounded) {
+                    _rb2D.velocity = new Vector2(input * _speed, _rb2D.velocity.y);
+                } else {
+                    _rb2D.velocity = new Vector2(input * _speed, _rb2D.velocity.y);
+                }
+                if (_rb2D.velocity.y < 0 && !_isGrounded) {
+                    _rb2D.gravityScale = 2f;
+                } else {
+                    _rb2D.gravityScale = 1f;
+                }
             } else {
-                _rb2D.gravityScale = 1f;
+                _rb2D.velocity = new Vector2(_rb2D.velocity.x, _rb2D.velocity.y);
+                _rb2D.gravityScale = 0.25f;
             }
         }
 
         private bool GetGrounded() {
-            return Physics2D.BoxCast(_rb2D.position, _renderer.size, 0f, Vector2.down, 0.04f, _ground);
+            RaycastHit2D hit = Physics2D.BoxCast(_rb2D.position, _renderer.size, 0f, Vector2.down, 0.04f, _ground);
+            if (hit) {
+                return hit.normal.y >= 0.75f;
+            } else {
+                return false;
+            }
         }
     }
 }
