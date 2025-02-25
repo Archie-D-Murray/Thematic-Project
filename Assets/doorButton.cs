0a6442764f04fcadecc2d05ff808c20cf31e5868
Ex-4 <146730152+Ex-4@users.noreply.github.com>
2025-02-25 11:36:28 +0000

    butt on

--- Assets/doorButton.cs
+++ Assets/doorButton.cs
@@ -1,20 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
 
 public class doorButton : MonoBehaviour
 {
 
     Door door;
+    Boolean canInteract;
+
     // Start is called before the first frame update
     void Start()
     {
+        canInteract = false;
         door = transform.parent.GetComponent<Door>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        door.toggle();
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.E) && canInteract)  {
+            door.toggle();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            canInteract = true;        
+            }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            canInteract = false;
+        }
     }
 }
