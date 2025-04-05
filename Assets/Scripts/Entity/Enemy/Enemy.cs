f082aef8e7c5227482fd44c337abafe054fc5b74
Ex-4 <146730152+Ex-4@users.noreply.github.com>
2025-04-05 17:36:01 +0100

    rework complete

--- Assets/Scripts/Entity/Enemy/Enemy.cs
+++ Assets/Scripts/Entity/Enemy/Enemy.cs
@@ -80,7 +80,7 @@ public abstract class Enemy : Placeable {
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collider.TryGetComponent(out PlayerController player)) {
+        if (collision.collider.gameObject.TryGetComponent(out PlayerController player)) {
             print("player");
             if (player.IsVulnerable()) {
                 player.OnDeath();
