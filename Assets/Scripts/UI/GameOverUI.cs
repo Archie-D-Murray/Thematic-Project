c14a91e7794f6d3f3203c51ccb30430040847425
Ex-4 <146730152+Ex-4@users.noreply.github.com>
2025-04-08 14:25:54 +0100

    game over menu

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -1,28 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+
+using TMPro;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField] Button RestartButton;
     [SerializeField] Button MenuButton;
+    [SerializeField] int MainMenuIndex;
+    [SerializeField] string TitleText;
         
     // Start is called before the first frame update
     void Start()
     {
+        GetComponentInChildren<TextMeshPro>().text = TitleText;
+        enabled = true;
         RestartButton.onClick.AddListener(() => Restart());
         MenuButton.onClick.AddListener(() => Menu());
     }
 
-    private void Menu() {
+    public void Menu() {
         //Load Main Menu scene
-        print("Back to Menu");
+        SceneManager.LoadScene(MainMenuIndex);
     }
 
-    private void Restart() {
+    public void Restart() {
         //Reload level data here
         print("restart");
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
+    }
+
+    public void PopUp() {
+        enabled = true;
     }
 
     //TODO
